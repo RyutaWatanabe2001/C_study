20a9e0de65e1e14006562e4b4bb2910fc74225b6
RyutaWatanabe2001 <danceryuta1201@icloud.com>
2021-01-19 11:59:55 +0900

    Udemyで関数について学びました
    講座を受けたため
     Please enter the commit message for your changes. Lines starting

--- culculate.c
+++ culculate.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+double avg(double a, double b) {
+  double c = (a + b) / 2.0;
+  return c;
+}
+
 int main(void)
 {
-  int suuti;
-  scanf("%d",&suuti );
-  if(suuti == 10)printf("入力値は10です。\n");
-  if(suuti != 10)printf("入力値は10ではありません。\n");
+    double d1, d2, d3;
+    double x = 1.2, y = 3.4, z = 5.4;
+    //同じ計算が3回
+    d1 = avg(x,y);
+    d2 = avg(9.3,8.3);
+    d3 = avg(z,x);
+    printf("d1 = %f,d2 = %f,d3 = %f\n", d1,d2,d3 );
 
-  return 0;
+    return 0;
+
 }
